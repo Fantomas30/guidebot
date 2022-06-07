87ea3fd3332508bceda3787db962ce71c210f14e
Fantomas30 <100239699+Fantomas30@users.noreply.github.com>
2022-06-07 16:16:45 +0200

    Update pica.js

--- commands/pica.js
+++ commands/pica.js
@@ -3,17 +3,13 @@ const { codeBlock } = require("@discordjs/builders");
 const { DurationFormatter } = require("@sapphire/time-utilities");
 const durationFormatter = new DurationFormatter();
 
-exports.run = (client, message, args, level) => { // eslint-disable-line no-unused-vars
-  const duration = durationFormatter.format(client.uptime);
-  const stats = codeBlock("asciidoc", `= STATISTICS =
-  • Mem Usage  :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
-  • Uptime     :: ${duration}
-  • Users      :: ${client.guilds.cache.map(g => g.memberCount).reduce((a, b) => a + b).toLocaleString()}
-  • Servers    :: ${client.guilds.cache.size.toLocaleString()}
-  • Channels   :: ${client.channels.cache.size.toLocaleString()}
-  • Discord.js :: v${version}
-  • Node       :: ${process.version}`);
-  message.channel.send(stats);
+if (!channel.parent) {
+	return console.log('This channel is not listed under a category');
+}
+
+channel.lockPermissions()
+	.then(() => console.log('Successfully synchronized permissions with parent channel'))
+	.catch(console.error);
 };
 
 exports.conf = {
