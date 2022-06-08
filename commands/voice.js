81b39d2d46106361bd514ddaadb603b95f759dc3
Fantomas30 <vojtavojta948@gmail.com>
2022-06-08 11:10:04 +0200

    xddd

--- commands/voice.js
+++ commands/voice.js
@@ -1,13 +1,7 @@
-.on("ready", () => {
-  const channel = client.channels.get("mychannelid");
-  if (!channel) return console.error("The channel does not exist!");
-  channel.join().then(connection => {
-    // Yay, it worked!
-    console.log("Successfully connected.");
-  }).catch(e => {
-    // Oh no, it errored! Let's log it to console :)
-    console.error(e);
-  });
+client.on('message', message => {
+  if (message.content === '+ping') {  
+    message.channel.send(`🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
+  }
 });
 
 exports.conf = {
